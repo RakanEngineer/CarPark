dc09e77caefc6951b74279ae96ba5872fbca384c
RakanEngineer <rakan.engineer@gmail.com>
2024-10-16 15:50:15 +0200

    Add and update ParkingForCarParkApp

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace CarPark
     class Program
     {
         //static string connectionString= "Server=.;Database=Parkeringshus;Integrated Security=true";
-        static string connectionString = "Server=.;Initial Catalog=CarPark;Integrated Security=true";
+        static string connectionString = "Server=.;Initial Catalog=CarPark;Integrated Security=true; Encrypt=True;Trust Server Certificate=True";
         //static string connectionString = "Data Source=.;Initial Catalog=Parkeringshus;Integrated Security=true";
         static void Main(string[] args)
         {
@@ -42,13 +42,27 @@ namespace CarPark
                             string notes = ReadLine();
                             DateTime arrival = DateTime.Now;
 
-                           
+                            RegisterArrival(
+                                firstName,
+                                lastName,
+                                socialSecurityNumber,
+                                phoneNumber,
+                                email,
+                                registrationNumber,
+                                notes,
+                                arrival);
+                            Clear();
+                            WriteLine("Arrival registered");
+                            Thread.Sleep(2000);
+                            break;
                         }
-                        break;
                     case ConsoleKey.D2:
                     case ConsoleKey.NumPad2:
                         {
-                            
+                            Write("Car: ");
+                            string registrationNumber = ReadLine();
+                            DateTime departure = DateTime.Now;
+                            RegisterDeparture(registrationNumber, departure);
                         }
                         Clear();
                         // TODO: create notice snippet
@@ -58,6 +72,7 @@ namespace CarPark
                     case ConsoleKey.D3:
                     case ConsoleKey.NumPad3:
                         {
+                            ShowParkingRegistry();
                             break;
                         }
                     case ConsoleKey.D4:
@@ -68,5 +83,86 @@ namespace CarPark
                 Clear();
             }
         }
+        private static void ShowParkingRegistry()
+        {
+            WriteLine("ID   Registration number       Arrived at       Departed at");
+            WriteLine("--------------------------------------------------------------");
+            string query = @"SELECT Id, 
+                                    FirstName,
+                                    LastName,
+                                    SocialSecurityNumber,
+                                    PhoneNumber,
+                                    Email,
+                                    RegistrationNumber, 
+                                    Notes, 
+                                    ArrivedAt, 
+                                    DepartedAt
+                               FROM ParkingForCarParkApp";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                string id = dataReader["Id"].ToString();
+                string registrationNumber = dataReader["RegistrationNumber"].ToString();
+                string arrivedAt = dataReader["ArrivedAt"].ToString();
+                string departedAt = dataReader["DepartedAt"].ToString();
+                Write(id.PadRight(5, ' '));
+                Write(registrationNumber.PadRight(24, ' '));
+                Write(arrivedAt.PadRight(20, ' '));
+                WriteLine(departedAt);
+            }
+            connection.Close();
+            WriteLine();
+            WriteLine("<Press key to continue");
+            ReadKey(true);
         }
+        private static void RegisterDeparture(string registrationNumber, DateTime departedAt)
+        {
+            string query = @"UPDATE ParkingForCarParkApp 
+                                SET DepartedAt = @DepartedAt
+                              WHERE RegistrationNumber = @RegistrationNumber
+                                AND DepartedAt IS NULL";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("DepartedAt", departedAt);
+            command.Parameters.AddWithValue("RegistrationNumber", registrationNumber);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        private static void RegisterArrival(string firstName, string lastName, string socialSecurityNumber, string phoneNumber, string email, string registrationNumber, string notes, DateTime arrivedAt)
+        {
+            string query = @"INSERT INTO ParkingForCarParkApp (FirstName, 
+                                                  LastName, 
+                                                  SocialSecurityNumber,
+                                                  PhoneNumber,
+                                                  Email,
+                                                  RegistrationNumber, 
+                                                  Notes, 
+                                                  ArrivedAt)
+                                          VALUES (@FirstName,
+                                                  @LastName,
+                                                  @SocialSecurityNumber, 
+                                                  @PhoneNumber,
+                                                  @Email, 
+                                                  @RegistrationNumber, 
+                                                  @Notes, 
+                                                  @ArrivedAt)";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@FirstName", firstName);
+            command.Parameters.AddWithValue("@LastName", lastName);
+            command.Parameters.AddWithValue("@SocialSecurityNumber", socialSecurityNumber);
+            command.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+            command.Parameters.AddWithValue("@Email", email);
+            command.Parameters.AddWithValue("@RegistrationNumber", registrationNumber);
+            command.Parameters.AddWithValue("@Notes", notes);
+            command.Parameters.AddWithValue("@ArrivedAt", arrivedAt);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
 }
